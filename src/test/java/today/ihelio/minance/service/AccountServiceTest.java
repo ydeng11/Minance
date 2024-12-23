4e82cc99d09e189357032ed398d34b6a5869ab75
ydeng <dengyujun11@gmail.com>
2024-12-23 00:33:17 -0500

    chore: fix tests

--- src/test/java/today/ihelio/minance/service/AccountServiceTest.java
+++ src/test/java/today/ihelio/minance/service/AccountServiceTest.java
@@ -2,10 +2,11 @@ package today.ihelio.minance.service;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import today.ihelio.jooq.tables.pojos.Accounts;
 import today.ihelio.minance.exception.CustomException;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,61 +14,66 @@ import static today.ihelio.minance.csvpojos.BankAccountPair.BankName.CITI;
 
 @QuarkusTest
 public class AccountServiceTest {
-  @Inject AccountService accountService;
-  @Inject BankService bankService;
+	@Inject
+	AccountService accountService;
+	@Inject
+	BankService bankService;
 
-  @Test public void testAccountService_createAccount_illegalBank() throws Exception {
-    Accounts accounts = new Accounts();
-    accounts.setAccountName("test");
-    accounts.setBankName("citi");
-    accounts.setAccountType("credit");
-    accounts.setInitBalance(BigDecimal.valueOf(127.10));
+	@Test
+	public void testAccountService_createAccount_illegalBank() throws Exception {
+		Accounts accounts = new Accounts();
+		accounts.setAccountName("test");
+		accounts.setBankName("citi");
+		accounts.setAccountType("credit");
+		accounts.setInitBalance(BigDecimal.valueOf(127.10));
 
-    assertThatThrownBy(() -> accountService.create(accounts)).isInstanceOf(CustomException.class)
-        .hasCauseInstanceOf(
-            IllegalArgumentException.class)
-        .hasMessageContaining("citi is not allowed!");
-  }
+		assertThatThrownBy(() -> accountService.create(accounts)).isInstanceOf(CustomException.class)
+				.hasCauseInstanceOf(
+						IllegalArgumentException.class)
+				.hasMessageContaining("citi is not allowed!");
+	}
 
-  @Test public void testAccountService_createAccount_bankNotExisting() throws Exception {
-    Accounts accounts = new Accounts();
-    accounts.setAccountName("test");
-    accounts.setBankName("invalid");
-    accounts.setAccountType("CREDIT");
-    accounts.setInitBalance(BigDecimal.valueOf(127.10));
+	@Test
+	public void testAccountService_createAccount_bankNotExisting() throws Exception {
+		Accounts accounts = new Accounts();
+		accounts.setAccountName("test");
+		accounts.setBankName("invalid");
+		accounts.setAccountType("CREDIT");
+		accounts.setInitBalance(BigDecimal.valueOf(127.10));
 
-    assertThatThrownBy(() -> accountService.create(accounts)).isInstanceOf(CustomException.class)
-        .hasCauseInstanceOf(
-            IllegalArgumentException.class)
-        .hasMessageContaining("invalid is not allowed!");
-  }
+		assertThatThrownBy(() -> accountService.create(accounts)).isInstanceOf(CustomException.class)
+				.hasCauseInstanceOf(
+						IllegalArgumentException.class)
+				.hasMessageContaining("invalid is not allowed!");
+	}
 
-  @Test public void testAccountService_createUpdateDeleteAccount_legalBank() throws Exception {
-    Accounts accounts = new Accounts();
-    accounts.setAccountName("test");
-    accounts.setBankName("CITI");
-    accounts.setAccountType("CREDIT");
-    BigDecimal balance = new BigDecimal("123.12");
-    accounts.setInitBalance(balance);
-    bankService.create(CITI);
-    accountService.create(accounts);
-    assertThat(accountService.retrieveAccountsByBank(CITI).size()).isEqualTo(1);
-    Accounts newAccounts = accountService.retrieveAccountsByBank(CITI).get(0);
-    assertThat(newAccounts.getAccountName()).isEqualTo(
-        "test");
-    assertThat(newAccounts.getInitBalance()).isEqualTo(
-        balance);
+	@Test
+	public void testAccountService_createUpdateDeleteAccount_legalBank() throws Exception {
+		Accounts accounts = new Accounts();
+		accounts.setAccountName("test");
+		accounts.setBankName("CITI");
+		accounts.setAccountType("CREDIT");
+		BigDecimal balance = new BigDecimal("123.12");
+		accounts.setInitBalance(balance);
+		bankService.create(CITI);
+		accountService.create(accounts);
+		assertThat(accountService.retrieveAccountsByBank(CITI).size()).isEqualTo(1);
+		Accounts newAccounts = accountService.retrieveAccountsByBank(CITI).getFirst();
+		assertThat(newAccounts.getAccountName()).isEqualTo(
+				"test");
+		assertThat(newAccounts.getInitBalance()).isEqualTo(
+				balance);
 
-    BigDecimal newBalance = new BigDecimal("999.12");
-    newAccounts.setInitBalance(newBalance);
+		BigDecimal newBalance = new BigDecimal("999.12");
+		newAccounts.setInitBalance(newBalance);
 
-    accountService.update(newAccounts);
-    assertThat(accountService.retrieveAccountsByBank(CITI).get(0).getInitBalance()).isEqualTo(
-        newBalance);
+		accountService.update(newAccounts);
+		assertThat(accountService.retrieveAccountsByBank(CITI).get(0).getInitBalance()).isEqualTo(
+				newBalance);
 
-    assertThat(accountService.retrieveAll().size()).isEqualTo(1);
+		assertThat(accountService.retrieveAll().size()).isEqualTo(1);
 
-    accountService.delete(newAccounts.getAccountId());
-    assertThat(accountService.retrieveAll().size()).isEqualTo(0);
-  }
+		accountService.delete(newAccounts.getAccountId());
+		assertThat(accountService.retrieveAll().size()).isEqualTo(0);
+	}
 }
--- src/test/java/today/ihelio/minance/service/BankServiceTest.java
+++ src/test/java/today/ihelio/minance/service/BankServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import today.ihelio.jooq.tables.pojos.Banks;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static today.ihelio.minance.csvpojos.BankAccountPair.BankName.CITI;
 
@@ -31,7 +33,7 @@ public class BankServiceTest {
 
 	@Test
 	public void testBankService_CreateDeleteBanks() throws Exception {
-		assertThat(bankService.create(CITI)).isEqualTo(1);
+		assertThat(bankService.create(CITI)).isPresent();
 
 		assertThat(bankService.retrieveAll().size()).isEqualTo(1);
 
@@ -42,17 +44,17 @@ public class BankServiceTest {
 
 	@Test
 	public void testBankService_RetrieveBanks() throws Exception {
-		assertThat(bankService.create(CITI)).isEqualTo(1);
+		assertThat(bankService.create(CITI)).isPresent();
 
-		Banks citi = bankService.findBankByName(CITI);
+		Banks citi = bankService.findBankByName(CITI).orElseThrow();
 		assertThat(citi.getBankName()).isEqualTo(CITI.getName());
 
-		assertThat(bankService.retrieve(citi.getBankId())).isEqualTo(citi);
+		assertThat(bankService.retrieve(citi.getBankId())).contains(citi);
 	}
 
 	@Test
 	public void testBankService_RetrieveNonexistentBanks() throws Exception {
-		Banks citi = bankService.findBankByName(CITI).get();
-		assertThat(citi).isNull();
+		Optional<Banks> citi = bankService.findBankByName(CITI);
+		assertThat(citi).isEmpty();
 	}
 }
