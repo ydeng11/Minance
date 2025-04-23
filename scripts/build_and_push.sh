af37f0b8e70708b674094d3d57d165b4be8650ff
ydeng11 <eugene.deng2077@gmail.com>
2025-04-23 00:44:58 -0400

    chore: make multi-platform docker in the script

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -9,7 +9,7 @@ echo "Building application..."
 
 # Build Docker image
 echo "Building Docker image..."
-docker build -f src/main/docker/Dockerfile.jvm -t minance:latest .
+docker build --platform linux/amd64,linux/arm64 -f src/main/docker/Dockerfile.jvm -t minance:latest .
 
 # Tag with version from pom.xml
 docker tag minance:latest ydeng11/minance:latest
